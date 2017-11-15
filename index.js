3214d632b5ba291b19f545184fdd4336dec66828
James Yu <jamesyu6149@gmail.com>
2017-11-15 05:32:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const newID = 'James';/*
+/*
 Variables defined in the test suite:
 - oldID
 - currentAge
@@ -7,3 +7,5 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+const newID = 1000000000 + oldiD;
